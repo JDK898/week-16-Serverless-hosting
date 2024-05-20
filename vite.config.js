1ef683c91f250c537f3e5d80d9a126835559ade5
JDK898 <james.daniel.king06@gmail.com>
2024-05-21 09:07:24 +1000

    Updated Vite again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/week-16-Serverless-hosting/",
+  base: "/week-16-Serverless-hosting",
   plugins: [react()],
 })
